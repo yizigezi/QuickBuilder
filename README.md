920a426a79317cbc6a7617811388d7dcd42738ac
yizigezi <54109315+yizigezi@users.noreply.github.com>
2025-02-04 23:11:33 +0800

    v1

---
+++ README.md
@@ -0,0 +1,13 @@
+# QuickBuilder
+
+for Vela JS App projects.
+
+更简单, 更易上手, 功能更丰富的的 Vela 编译工具(需搭配官方aiot-toolkit使用)
+
+## ✨Features
+
+* Support multi DisplayWidth build workflow
+* Build info injection
+* Smart version management
+* Full aiot-toolkit support
+* Github Integration(Coming soon)
---
+++ src/BuildAPI/Vela/build.py
@@ -0,0 +1,20 @@
+# Vela JS App BuildAPI
+
+class buildProcess:
+    def __init__(self, 
+                 designWidth: list, 
+                 jsc:bool=False, 
+                 custom_component:bool=False, 
+                 protobuf:bool=False,
+                 env:str='debug'):
+        self.designWidth = designWidth
+        self.buildMode = env
+        self.buildProps = []
+        if jsc:
+            self.buildProps.append('--enable-jsc')
+        if custom_component:
+            self.buildProps.append('--enable-custom-component')
+        if protobuf:
+            self.buildProps.append('--enable-protobuf')
+
+    def 
